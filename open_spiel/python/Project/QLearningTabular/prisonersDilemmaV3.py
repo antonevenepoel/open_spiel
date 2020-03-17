1a51c3f3d301551f818d5da25123c296c229a154
antonevenepoel <36889203+antonevenepoel@users.noreply.github.com>
2020-03-17 10:56:27 +0100

    Update prisonersDilemmaV3.py

--- open_spiel/python/Project/QLearningTabular/prisonersDilemmaV3.py
+++ open_spiel/python/Project/QLearningTabular/prisonersDilemmaV3.py
@@ -47,30 +47,83 @@ def create_environment(game):
 
 
 
-def execute_scenarios(env):
-    scenario1(env)
+def execute_scenarios(env, nb):
+    sumScenario1 = np.zeros(4)
+    for _ in range(nb):
+        rewardCounter(sumScenario1 , scenario1(env))
+    logging.info("The results of scenario 1 are: %s", sumScenario1)
 
+    sumScenario2 = np.zeros(4)
+    for _ in range(nb):
+        rewardCounter(sumScenario2, scenario2(env))
+    logging.info("The results of scenario 2 are: %s", sumScenario2)
+
+    sumScenario3 = np.zeros(4)
+    for _ in range(nb):
+        rewardCounter(sumScenario3, scenario3(env))
+    logging.info("The results of scenario 2 are: %s", sumScenario3)
+
+
+"""
+Scenario 1: 2 agents are trained with tabular_Q_learning against a random agent and evaluated against each other.
+"""
 def scenario1(env):
     agents = [
-        tabular_qlearner.QLearner(player_id=idx, num_actions=env.num_actions_per_step,)
+        tabular_qlearner.QLearner(player_id=idx, num_actions=env.num_actions_per_step, )
         for idx in range(env.num_players)
     ]
-
     random_agents = [
         random_agent.RandomAgent(player_id=idx, num_actions=env.num_actions_per_step, )
         for idx in range(env.num_players)
     ]
     train_agents(env, [agents[0], random_agents[0]], 1000)
-    env.reset()
     train_agents(env, [agents[1], random_agents[1]], 1000)
 
-    print(evaluate_agents(env, agents))
+    return evaluate_agents(env, agents)
+
+"""
+Scenario 2: 2 agents are trained with tabular_Q_learning against each other and evaluated against each other.
+"""
+def scenario2(env):
+    agents = [
+        tabular_qlearner.QLearner(player_id=idx, num_actions=env.num_actions_per_step, step_size=0.5, epsilon=0.2, discount_factor=1.0 )
+        for idx in range(env.num_players)
+    ]
+    train_agents(env, agents, 1000)
+    return evaluate_agents(env, agents)
+
+"""
+Scenario 3: 2 random against play against each other.
+"""
+
+def scenario3(env):
+    random_agents = [
+        random_agent.RandomAgent(player_id=idx, num_actions=env.num_actions_per_step, )
+        for idx in range(env.num_players)
+    ]
+
+    return evaluate_agents(env, random_agents)
+
+
+
+def rewardCounter(totalSum, reward):
+    payOffMatrix = [[3,3],[0,5]], [[5,0],[1,1]]
+    if reward == payOffMatrix[0][0]:
+        totalSum[0] +=1
+    elif reward == payOffMatrix[0][1]:
+        totalSum[1] +=1
+    elif reward == payOffMatrix[1][0]:
+        totalSum[2] +=1
+    elif reward == payOffMatrix[1][1]:
+        totalSum[3] +=1
+
+
 
 def main(_):
     game = create_game()
     env = create_environment(game)
-    for i in range(100):
-        execute_scenarios(env)
+    print(env.is_turn_based)
+    execute_scenarios(env, 10)
 
 
 if __name__ == "__main__":
