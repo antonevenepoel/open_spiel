d9cd9c376c14f1bfc71236e6ce2d2708b215a89b
Arnout Hillen <arnouthillen@gmail.com>
2020-04-15 12:15:39 +0200

    cfr en nfsp plots

--- open_spiel/python/Project/part_2/nfsp.py
+++ open_spiel/python/Project/part_2/nfsp.py
@@ -18,31 +18,20 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from absl import app
-from absl import flags
-from absl import logging
+import os
+# Disable all tensorflow warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow
+tensorflow.get_logger().setLevel('ERROR')
 import tensorflow.compat.v1 as tf
+from absl import logging
 
 from open_spiel.python import policy
 from open_spiel.python import rl_environment
 from open_spiel.python.algorithms import exploitability
 from open_spiel.python.algorithms import nfsp
-
-FLAGS = flags.FLAGS
-
-flags.DEFINE_integer("num_train_episodes", int(3e6),
-                     "Number of training episodes.")
-flags.DEFINE_integer("eval_every", 10000,
-                     "Episode frequency at which the agents are evaluated.")
-flags.DEFINE_list("hidden_layers_sizes", [
-    128,
-], "Number of hidden units in the avg-net and Q-net.")
-flags.DEFINE_integer("replay_buffer_capacity", int(2e5),
-                     "Size of the replay buffer.")
-flags.DEFINE_integer("reservoir_buffer_capacity", int(2e6),
-                     "Size of the reservoir buffer.")
-flags.DEFINE_float("anticipatory_param", 0.1,
-                   "Prob of using the rl best response as episode policy.")
+import matplotlib.pyplot as plt
+from open_spiel.python.project.part_1.visualization import paths
 
 
 class NFSPPolicies(policy.Policy):
@@ -74,40 +63,60 @@ class NFSPPolicies(policy.Policy):
     return prob_dict
 
 
-def main(unused_argv):
-  game = "leduc_poker"
-  num_players = 2
+def train_nfsp(
+        game="kuhn_poker",
+        num_players=2,
+        hidden_layers_sizes=(64,),
+        replay_buffer_capacity=int(2e5),
+        num_train_episodes=int(1e4),
+        epsilon_start=0.06,
+        epsilon_end=0.001,
+        reservoir_buffer_capacity=int(2e6),
+        anticipatory_param=0.1,
+        eval_every=int(1e4),
+) -> dict:
+
+  data = {}
+  data["game"] = game
+  data["episodes"], data["losses"], data["exploitability"] = [], [], []
 
   env_configs = {"players": num_players}
   env = rl_environment.Environment(game, **env_configs)
   info_state_size = env.observation_spec()["info_state"][0]
   num_actions = env.action_spec()["num_actions"]
 
-  hidden_layers_sizes = [int(l) for l in FLAGS.hidden_layers_sizes]
+  hidden_layers_sizes = [int(l) for l in hidden_layers_sizes] # FLAGS.hidden_layers_sizes]
   kwargs = {
-      "replay_buffer_capacity": FLAGS.replay_buffer_capacity,
-      "epsilon_decay_duration": FLAGS.num_train_episodes,
-      "epsilon_start": 0.06,
-      "epsilon_end": 0.001,
+      "replay_buffer_capacity":  replay_buffer_capacity, # FLAGS.replay_buffer_capacity,
+      "epsilon_decay_duration": num_train_episodes, # FLAGS.num_train_episodes,
+      "epsilon_start": epsilon_start, # FLAGS.epsilon_start,
+      "epsilon_end": epsilon_end # FLAGS.epsilon_end,
   }
 
   with tf.Session() as sess:
     # pylint: disable=g-complex-comprehension
     agents = [
         nfsp.NFSP(sess, idx, info_state_size, num_actions, hidden_layers_sizes,
-                  FLAGS.reservoir_buffer_capacity, FLAGS.anticipatory_param,
+                  reservoir_buffer_capacity, anticipatory_param,
+                  # FLAGS.reservoir_buffer_capacity, FLAGS.anticipatory_param,
                   **kwargs) for idx in range(num_players)
     ]
     expl_policies_avg = NFSPPolicies(env, agents, nfsp.MODE.average_policy)
-
     sess.run(tf.global_variables_initializer())
-    for ep in range(FLAGS.num_train_episodes):
-      if (ep + 1) % FLAGS.eval_every == 0:
+    for ep in range(num_train_episodes): # FLAGS.num_train_episodes):
+      if (ep + 1) % eval_every == 0: # FLAGS.eval_every == 0:
         losses = [agent.loss for agent in agents]
         logging.info("Losses: %s", losses)
         expl = exploitability.exploitability(env.game, expl_policies_avg)
-        logging.info("[%s] Exploitability AVG %s", ep + 1, expl)
-        logging.info("_____________________________________________")
+        print("Exploitability AVG", ep, expl)
+        print("_____________________________________________")
+        # logging.info("[%s] Exploitability AVG %s", ep + 1, expl)
+        # logging.info("_____________________________________________")
+
+        # store info
+        data["exploitability"].append(expl)
+        data["losses"].append(losses)
+        data["episodes"].append(ep + 1)
 
       time_step = env.reset()
       while not time_step.last():
@@ -120,6 +129,25 @@ def main(unused_argv):
       for agent in agents:
         agent.step(time_step)
 
+  sess.close()
+  return data
 
-if __name__ == "__main__":
-  app.run(main)
+
+output = {}
+if __name__ == '__main__':
+  output = train_nfsp(
+    hidden_layers_sizes=[128, 128, 64],
+    eval_every=int(5e3),
+    num_train_episodes=int(1e5)
+  )
+
+  # plots
+  plt.title("NFSP: " + output["game"], fontweight="bold")
+  plt.xlabel("Episodes", fontweight="bold")
+  plt.ylabel("Exploitability", fontweight="bold")
+  plt.loglog()
+  plt.plot(output["episodes"], output["exploitability"])
+  plt.savefig(paths.path_arnout
+              + 'nfsp_' + output["episodes"][-1] + '_episodes'
+              + '.' + paths.type)
+  plt.show()
---
+++ open_spiel/python/Project/part_2/paths.py
@@ -0,0 +1,5 @@
+# True for `path_arnout`, False for `path_anton`
+path_flag = True
+path_arnout = "/Users/arnouthillen/open_spiel/open_spiel/python/project/part_2/plots/"
+path_anton = "..." # aanvullen
+type = "png"
