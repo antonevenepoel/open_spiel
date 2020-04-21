b0437da80eb1d83d3f0b647ee68bc3ed6720d4b3
Anton Evenpoel <anton.evenepoel@icloud.com>
2020-04-21 15:56:19 +0200

    commit extra results

--- open_spiel/python/Project/part_2/paths.py
+++ open_spiel/python/Project/part_2/paths.py
@@ -1,7 +1,7 @@
-PATH_FLAG = False # zet hier op True als je jouw pad wilt gebruiken
+PATH_FLAG = True # zet hier op True als je jouw pad wilt gebruiken
 
 path_arnout = "/Users/arnouthillen/open_spiel/open_spiel/python/project/part_2"
-path_anton = "..." # invullen
+path_anton = "/Users/antonevenepoel/Github/open_spiel2/open_spiel/python/project/part_2" # invullen
 
 path = path_arnout if not PATH_FLAG else path_anton
 
